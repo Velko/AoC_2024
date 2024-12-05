849288d587beeb6f9f730b0f2ba9123d9ad76bda
Jurģis Brigmanis <velko.x@gmail.com>
2024-12-05 22:37:45 +0200

    Add testing to boiler

--- mkday.sh
+++ mkday.sh
@@ -10,12 +10,15 @@ DAY=$1
 cargo new day$DAY
 touch inputs/in_$DAY.txt
 touch inputs/sample_$DAY.txt
+cp boiler/tests.txt inputs/tests_$DAY.txt
 touch puzzles/$DAY.txt
+
 
 cd day$DAY
 
 ln -s ../inputs/in_$DAY.txt input.txt
 ln -s ../inputs/sample_$DAY.txt sample.txt
+ln -s ../inputs/tests_$DAY.txt tests.txt
 ln -s ../puzzles/$DAY.txt readme.txt
 
 cargo add --path ../aoc_tools/
