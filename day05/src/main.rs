853a52b27196772010bcf9d415f1928a6c935021
Jurģis Brigmanis <velko.x@gmail.com>
2024-12-05 10:01:04 +0200

    Solved Day05p1

---
+++ day05/src/main.rs
@@ -0,0 +1,113 @@
+use aoc_tools::{IterMoreTools, InvalidInput, ResultExt};
+use itertools::Itertools;
+
+fn main() -> anyhow::Result<()> {
+    let input = aoc_tools::Input::from_cmd()?.read_lines()?;
+
+    let rules = input
+        .iter()
+        .take_while(|l| !l.is_empty())
+        .map(parse_rules)
+        .try_collect_vec()?;
+
+    let updates = input
+        .iter()
+        .skip_while((|l| !l.is_empty()))
+        .skip(1)
+         .map(parse_update)
+         .try_collect_vec()?;
+
+    // println!("{:?}", rules);
+    // println!("{:?}", updates);
+
+    let safe_updates: Vec<_> = updates
+        .iter()
+        .filter(|u|is_update_safe(u, &rules))
+        .collect();
+
+//    println!("{:?}", safe_updates);
+
+    let result1: u32 = safe_updates
+        .iter()
+        .map(|&mp| extract_middle_page(mp))
+        .sum();
+
+    println!("Result p1: {}", result1);
+
+    let result2 = 0;
+    println!("Result p2: {}", result2);
+
+    Ok(())
+}
+
+fn parse_rules<S: AsRef<str>>(dim: S) -> Result<(u32, u32), InvalidInput>
+    where S: Into<String>{
+    let r = dim.as_ref();
+    let parsed =
+        r
+            .split('|')
+            .map(str::parse::<u32>)
+            .try_collect_vec()
+            .map_err_to_invalid_input(r)?;
+
+    let res: (u32, u32) = parsed
+        .into_iter()
+        .collect_tuple()
+        .map_err_to_invalid_input(r)?;
+
+    Ok(res)
+}
+
+fn parse_update<S: AsRef<str>>(dim: S) -> Result<Vec<u32>, InvalidInput>
+    where S: Into<String>{
+    let r = dim.as_ref();
+    let parsed =
+        r
+            .split(',')
+            .map(str::parse::<u32>)
+            .try_collect_vec()
+            .map_err_to_invalid_input(r)?;
+
+    Ok(parsed)
+}
+
+fn is_update_safe(update: &Vec<u32>, rules: &Vec<(u32, u32)>) -> bool {
+    for (index, page) in update.iter().enumerate() {
+        let page_rules = rules
+            .iter()
+            .filter(|(p, _)| p == page)
+            .map(|(_, p)| p );
+        for chk_page in page_rules {
+            let chk_pos = update
+                .iter()
+                .position(|p| p == chk_page)
+                .unwrap_or(usize::MAX);
+
+            if chk_pos < index {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+fn extract_middle_page(update: &Vec<u32>) -> u32 {
+    assert!(update.len() & 1 == 1);
+    let mid_idx = update.len() / 2;
+
+    let val = update.get(mid_idx).unwrap();
+
+    *val
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_works() {
+
+        assert_eq!(1, 1);
+    }
+}
