0242b12eafdd9ea6eac25f01d0fbad6b16a764eb
Jurģis Brigmanis <velko.x@gmail.com>
2024-12-05 22:37:45 +0200

    Add testing to boiler

--- boiler/main.rs
+++ boiler/main.rs
@@ -1,22 +1,62 @@
 use aoc_tools::{IterMoreTools, InvalidInput, ResultExt};
 
+type ParsedInput = ();
+
 fn main() -> anyhow::Result<()> {
-    let result1 = 0;
+    let input = aoc_tools::Input::from_cmd()?;
+    let parsed = parse_input(input)?;
+
+    let result1 = calculate_p1(&parsed);
     println!("Result p1: {}", result1);
 
-    let result2 = 0;
+    let result2 = calculate_p2(&parsed);
     println!("Result p2: {}", result2);
 
     Ok(())
 }
 
+fn parse_input(_input: aoc_tools::Input) -> anyhow::Result<ParsedInput> {
+    todo!()
+}
+
+fn calculate_p1(_input: &ParsedInput) -> u64 {
+    todo!()
+}
+
+fn calculate_p2(_input: &ParsedInput) -> u64 {
+    0
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use aoc_tools::TestSamples;
+
+    fn load_sample(index: usize) -> anyhow::Result<(ParsedInput, u64)> {
+        let samples = TestSamples::try_new()?;
+        let (input, expected) = samples.get_sample(index)?;
+        let parsed = parse_input(input)?;
+        Ok((parsed, expected))
+    }
 
     #[test]
-    fn test_works() {
+    fn test_sample_p1() -> anyhow::Result<()> {
+        let (parsed, expected) = load_sample(0)?;
 
-        assert_eq!(1, 1);
+        let result1 = calculate_p1(&parsed);
+
+        assert_eq!(expected, result1);
+        Ok(())
+    }
+
+    #[test]
+    #[ignore]
+    fn test_sample_p2() -> anyhow::Result<()> {
+        let (parsed, expected) = load_sample(1)?;
+
+        let result2 = calculate_p2(&parsed);
+
+        assert_eq!(expected, result2);
+        Ok(())
     }
 }
