e05cec1b44dc77c3c836b71a6b38afb4a51f68ed
Jurģis Brigmanis <velko.x@gmail.com>
2024-12-09 12:21:25 +0200

    Solved Day09p2

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,4 +1,3 @@
-use aoc_tools::{IterMoreTools, InvalidInput, ResultExt};
 use std::iter::repeat_n;
 
 type ParsedInput = Vec<(Option<usize>, usize)>;
@@ -82,15 +81,88 @@ fn expand_disk_map(input: &ParsedInput) -> Vec<(Option<usize>, usize)> {
 fn calculate_disk_checksum(disk_map: &Vec<(Option<usize>, usize)>) -> u64 {
     disk_map
         .into_iter()
-        .filter_map(|(f, _)|*f)
         .enumerate()
-        .map(|(pos, file_id)| pos as u64 * file_id as u64)
+        .map(|(pos, (file_id, _))| pos as u64 * file_id.unwrap_or(0) as u64 )
         .sum()
 }
 
-fn calculate_p2(_input: &ParsedInput) -> u64 {
-    0
+fn calculate_p2(input: &ParsedInput) -> u64 {
+    let mut disk_map = expand_disk_map(input);
+
+    let mut block_idx = disk_map.len() - 1;
+
+    loop {
+        while disk_map.get(block_idx).unwrap().0.is_none() {
+            block_idx -= 1;
+        }
+
+        let block_size = disk_map.get(block_idx).unwrap().1;
+        block_idx -= block_size - 1;
+
+        //println!("{:?}", &disk_map.as_slice()[end_idx..end_idx + block_size]);
+
+        let mut free_idx = 0;
+
+        while free_idx < disk_map.len() && not_fit(disk_map.get(free_idx).unwrap(), block_size) {
+            free_idx += 1;
+        }
+
+
+        if free_idx < disk_map.len() && block_idx > free_idx {
+            let free_size = disk_map.get(free_idx).unwrap().1;
+
+            //println!("{:?}", &disk_map.as_slice()[free_idx..free_idx + free_size]);
+
+
+            move_block(&mut disk_map, free_idx, block_idx, block_size);
+            resize_remaining_free(&mut disk_map, free_idx + block_size, free_size - block_size);
+
+            //println!("{:?}\n", disk_map);
+            //print_map(&disk_map);
+        }
+        if block_idx == 0 {
+            break;
+        }
+        block_idx -= 1;
+
+        //println!("{}", 100 * (disk_map.len() - block_idx) / disk_map.len());
+    }
+
+    //print_map(&disk_map);
+
+    calculate_disk_checksum(&disk_map)
 }
+
+fn not_fit(block: &(Option<usize>, usize), wanted: usize) -> bool {
+    block.0.is_some() || block.1 < wanted
+}
+
+fn move_block(disk_map: &mut Vec<(Option<usize>, usize)>, free_idx: usize, block_idx: usize, block_size: usize) {
+    for i in 0..block_size {
+        *disk_map.get_mut(free_idx + i).unwrap()
+            = *disk_map.get(block_idx + i).unwrap();
+
+        *disk_map.get_mut(block_idx + i).unwrap() = (None, block_size);
+    }
+}
+
+fn resize_remaining_free(disk_map: &mut Vec<(Option<usize>, usize)>, free_idx: usize, free_size: usize) {
+    for i in 0..free_size {
+        *disk_map.get_mut(free_idx + i).unwrap() = (None, free_size);
+    }
+}
+
+
+// fn print_map(disk_map: &Vec<(Option<usize>, usize)>) {
+//     for (p, _) in disk_map.into_iter() {
+//         print!("{}", match p{
+//             Some(d) => format!("{}", d),
+//             None => ".".to_string(),
+//         });
+//     }
+
+//     println!();
+// }
 
 #[cfg(test)]
 mod tests {
@@ -115,7 +187,6 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn test_sample_p2() -> anyhow::Result<()> {
         let (parsed, expected) = load_sample(1)?;
 
