d694d052013e1eb99747449a552a66ae247a0ab7
Jurģis Brigmanis <velko.x@gmail.com>
2024-12-09 09:36:54 +0200

    Solved Day09p1

---
+++ day09/src/main.rs
@@ -0,0 +1,113 @@
+use aoc_tools::{IterMoreTools, InvalidInput, ResultExt};
+use std::iter::repeat_n;
+
+type ParsedInput = Vec<Option<usize>>;
+
+fn main() -> anyhow::Result<()> {
+    let input = aoc_tools::Input::from_cmd()?;
+    let parsed = parse_input(input)?;
+
+    let result1 = calculate_p1(&parsed);
+    println!("Result p1: {}", result1);
+
+    let result2 = calculate_p2(&parsed);
+    println!("Result p2: {}", result2);
+
+    Ok(())
+}
+
+fn parse_input(input: aoc_tools::Input) -> anyhow::Result<ParsedInput> {
+    let in_str = input.read_single_line()?;
+
+    // 2333133121414131402
+    let mut in_c = in_str.chars().into_iter();
+    let mut file_id = 0;
+
+    let mut disk_map: Vec<Option<usize>> = Vec::new();
+
+    while let Some(file_len) = in_c.next() {
+        disk_map.extend(repeat_n(Some(file_id), parse_char(file_len)));
+
+        file_id += 1;
+        if let Some(space_len) = in_c.next() {
+            disk_map.extend(repeat_n(None, parse_char(space_len)));
+        } else {
+            break;
+        }
+    }
+
+    Ok(disk_map)
+}
+
+fn parse_char(c: char) -> usize {
+    format!("{}", c).parse().unwrap()
+}
+
+fn calculate_p1(input: &ParsedInput) -> u64 {
+    let mut disk_map = input.clone();
+
+    let mut start_idx = 0;
+    let mut end_idx = disk_map.len() - 1;
+
+    while start_idx < end_idx {
+        while disk_map.get(start_idx).unwrap().is_some() {
+            start_idx += 1;
+        }
+
+        while disk_map.get(end_idx).unwrap().is_none() {
+            end_idx -= 1;
+        }
+
+        *disk_map.get_mut(start_idx).unwrap()
+            = *disk_map.get(end_idx).unwrap();
+        *disk_map.get_mut(end_idx).unwrap() = None;
+
+        start_idx += 1;
+        end_idx -= 1;
+    }
+
+    disk_map
+        .into_iter()
+        .filter_map(|f|f)
+        .enumerate()
+        .map(|(pos, file_id)| pos as u64 * file_id as u64)
+        .sum()
+}
+
+fn calculate_p2(_input: &ParsedInput) -> u64 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use aoc_tools::TestSamples;
+
+    fn load_sample(index: usize) -> anyhow::Result<(ParsedInput, u64)> {
+        let samples = TestSamples::try_new()?;
+        let (input, expected) = samples.get_sample(index)?;
+        let parsed = parse_input(input)?;
+        Ok((parsed, expected))
+    }
+
+    #[test]
+    fn test_sample_p1() -> anyhow::Result<()> {
+        let (parsed, expected) = load_sample(0)?;
+
+        let result1 = calculate_p1(&parsed);
+
+        assert_eq!(expected, result1 as u64);
+        Ok(())
+    }
+
+    #[test]
+    #[ignore]
+    fn test_sample_p2() -> anyhow::Result<()> {
+        let (parsed, expected) = load_sample(1)?;
+
+        let result2 = calculate_p2(&parsed);
+
+        assert_eq!(expected, result2 as u64);
+        Ok(())
+    }
+}
