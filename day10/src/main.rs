2ee7e6f1c8ece5a9f5f8f9d3aee26f27655b00ee
Jurģis Brigmanis <velko.x@gmail.com>
2024-12-10 09:57:06 +0200

    Solved Day10p2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -51,43 +51,68 @@ fn calculate_p1(input: &ParsedInput) -> usize {
 }
 
 
-fn walk_path_1(grid: &ParsedInput, (posx, posy): (usize, usize), width: usize, height: usize, current_step: char, reached: &mut HashSet<(usize, usize)>) {
+fn walk_path_1(grid: &ParsedInput, (posx, posy): (usize, usize), width: usize, height: usize, current_step: char, reached: &mut HashSet<(usize, usize)>) -> usize {
 
     //print!("{:?}, {}", (posx, posy), current_step);
 
     if *grid.get(posy).unwrap().get(posx).unwrap() != current_step {
         //println!("Exit");
-        return;
+        return 0;
     }
 
     if current_step == '9' {
         //println!("Score");
         reached.insert((posx, posy));
-        return;
+        return 1;
     }
 
     //println!();
 
+    let mut score = 0;
+
     let next_step = (current_step as u8 + 1) as char;
 
     if let Some(next_x) = posx.clamped_add_signed(1, width) {
-        walk_path_1(grid, (next_x, posy), width, height, next_step, reached);
+        score += walk_path_1(grid, (next_x, posy), width, height, next_step, reached);
     }
     if let Some(next_x) = posx.clamped_add_signed(-1, width) {
-        walk_path_1(grid, (next_x, posy), width, height, next_step, reached);
+        score += walk_path_1(grid, (next_x, posy), width, height, next_step, reached);
     }
     if let Some(next_y) = posy.clamped_add_signed(1, height) {
-        walk_path_1(grid, (posx, next_y), width, height, next_step, reached);
+        score += walk_path_1(grid, (posx, next_y), width, height, next_step, reached);
     }
     if let Some(next_y) = posy.clamped_add_signed(-1, height) {
-        walk_path_1(grid, (posx, next_y), width, height, next_step, reached);
+        score += walk_path_1(grid, (posx, next_y), width, height, next_step, reached);
     }
+
+    score
 }
 
 
 
-fn calculate_p2(_input: &ParsedInput) -> u64 {
-    0
+fn calculate_p2(input: &ParsedInput) -> usize {
+    let height = input.len();
+    let width = input.get(0).unwrap().len();
+
+    let mut starts: Vec<(usize, usize)> = Vec::new();
+
+    for (y, row) in input.iter().enumerate() {
+        for (x, cell) in row.into_iter().enumerate() {
+            if *cell == '0' {
+                starts.push((x, y));
+            }
+        }
+    }
+
+    //println!("{:?}", starts);
+
+
+    let mut reached: HashSet<(usize, usize)> = HashSet::new();
+
+    starts
+        .into_iter()
+        .map(|pos| walk_path_1(&input, pos, width, height, '0', &mut reached))
+        .sum()
 }
 
 #[cfg(test)]
@@ -134,7 +159,6 @@ mod tests {
 
 
     #[test]
-    #[ignore]
     fn test_sample_p2() -> anyhow::Result<()> {
         let (parsed, expected) = load_sample(1)?;
 
