bc44bd10caed4c15affd9eabfe919b28925fb6c7
Jurģis Brigmanis <velko.x@gmail.com>
2024-12-20 00:10:39 +0200

    Improve boiler in regards of error handling

--- boiler/main.rs
+++ boiler/main.rs
@@ -6,10 +6,10 @@ fn main() -> anyhow::Result<()> {
     let input = aoc_tools::Input::from_cmd()?;
     let parsed = parse_input(input)?;
 
-    let result1 = calculate_p1(&parsed);
+    let result1 = calculate_p1(&parsed)?;
     println!("Result p1: {}", result1);
 
-    let result2 = calculate_p2(&parsed);
+    let result2 = calculate_p2(&parsed)?;
     println!("Result p2: {}", result2);
 
     Ok(())
@@ -19,12 +19,12 @@ fn parse_input(_input: aoc_tools::Input) -> anyhow::Result<ParsedInput> {
     todo!()
 }
 
-fn calculate_p1(_input: &ParsedInput) -> u64 {
+fn calculate_p1(_input: &ParsedInput) -> anyhow::Result<u64> {
     todo!()
 }
 
-fn calculate_p2(_input: &ParsedInput) -> u64 {
-    0
+fn calculate_p2(_input: &ParsedInput) -> anyhow::Result<u64> {
+    Ok(0)
 }
 
 #[cfg(test)]
@@ -45,7 +45,7 @@ mod tests {
     //#[case(load_sample("input.txt")?)]
     fn test_sample_p1(#[case] (parsed, expected, _): (ParsedInput, Option<u64>, Option<u64>)) -> anyhow::Result<()> {
 
-        let result1 = calculate_p1(&parsed);
+        let result1 = calculate_p1(&parsed)?;
 
         assert_eq!(expected, Some(result1 as u64));
         Ok(())
@@ -57,7 +57,7 @@ mod tests {
     #[ignore]
     fn test_sample_p2(#[case] (parsed, _, expected): (ParsedInput, Option<u64>, Option<u64>)) -> anyhow::Result<()> {
 
-        let result2 = calculate_p2(&parsed);
+        let result2 = calculate_p2(&parsed)?;
 
         assert_eq!(expected, Some(result2 as u64));
         Ok(())
