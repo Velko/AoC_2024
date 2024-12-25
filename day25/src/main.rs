8af9f463a9d0a06b91a661adcb53a91db1474309
Jurģis Brigmanis <velko.x@gmail.com>
2024-12-25 13:03:48 +0200

    Solved Day25p1

--- day25/src/main.rs
+++ day25/src/main.rs
@@ -1,7 +1,7 @@
 use aoc_tools::{IterMoreTools, InvalidInput, ResultExt};
 use anyhow::anyhow;
 
-type ParsedInput = ();
+type ParsedInput = (Vec<[usize; 5]>, Vec<[usize; 5]>);
 
 fn main() -> anyhow::Result<()> {
     let input = aoc_tools::Input::from_cmd()?;
@@ -16,12 +16,51 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn parse_input(_input: aoc_tools::Input) -> anyhow::Result<ParsedInput> {
-    todo!()
+fn parse_input(input: aoc_tools::Input) -> anyhow::Result<ParsedInput> {
+    let lines = input.read_lines()?;
+
+    let mut locks: Vec<[usize; 5]> = Vec::new();
+    let mut keys: Vec<[usize; 5]> = Vec::new();
+
+
+    for block in lines.as_slice().split(|l| l == "") {
+        let b_type = block[0].chars().next().unwrap();
+        let mut counts = [0; 5];
+        for b_line in &block[1..block.len()-1] {
+            for (i, c) in b_line.chars().enumerate() {
+                if c == '#' {
+                    counts[i] += 1;
+                }
+            }
+        }
+
+        if b_type == '#' {
+            locks.push(counts);
+        } else {
+            keys.push(counts);
+        }
+    }
+
+    Ok((locks, keys))
 }
 
-fn calculate_p1(_input: &ParsedInput) -> anyhow::Result<u64> {
-    todo!()
+fn calculate_p1(input: &ParsedInput) -> anyhow::Result<usize> {
+    let (locks, keys) = input;
+
+    println!("L: {:?}\nK: {:?}", locks, keys);
+
+    let mut matches = 0;
+
+    for lock in locks.iter() {
+        for key in keys.iter() {
+            println!("{:?} vs {:?}", lock, key);
+            if lock.iter().zip(key.iter()).all(|(l, k)| l + k < 6) {
+                matches += 1;
+            }
+        }
+    }
+
+    Ok(matches)
 }
 
 fn calculate_p2(_input: &ParsedInput) -> anyhow::Result<u64> {
